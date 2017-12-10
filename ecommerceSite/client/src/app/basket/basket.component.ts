173b7de58e79c1cbd73e18063d657c739a6fde3b
darshan2494 <sanghavi.da@husky.neu.edu>
2017-12-10 18:14:40 -0500

    Basket Issue resolved
    Basket Issue resolved with delete  product functionality

--- ecommerceSite/client/src/app/basket/basket.component.ts
+++ ecommerceSite/client/src/app/basket/basket.component.ts
@@ -26,6 +26,8 @@ export class BasketComponent implements OnInit {
   users: User[] = [];
   products : Product[] = [];
   appState = 'default';
+  op; // ordr price with shipping
+  fp; //final price of component
 
   
   
@@ -57,20 +59,37 @@ export class BasketComponent implements OnInit {
     for(var i=0; i <userCartLength; i++ ){
         totalPrice=    totalPrice +  (this.currentUser.cart[i].productPrice * this.currentUser.cart[i].cartQuantity); 
     }
-    alert("final cart Price: " +totalPrice);
+    this.fp = totalPrice;
+    
 
   }
   orderSummary(){
     //display the order summary with shipping
       var orderPrice = 0;
       orderPrice = totalPrice + 10;
-      alert("Order Price :"+ orderPrice);
+      this.op = orderPrice;
+      
   }
   orderDiscount(){
    // calculate the discount 
     totalPrice = totalPrice - ( (totalPrice * 0.10) );
     
     this.orderSummary();
+  }
+  cartObjectDelete(productName: string , user : User)
+  {        
+      this.appState='edit';
+      this.userService.updateuser(productName , user)
+      .subscribe(
+          data => {
+               this.alertService.success('Product Deleted from Cart Successfully !!', true);
+               this.loadAllUsers();
+               
+          },
+         
+          error => {
+             this.alertService.error(error);
+          });
   }
   
 }
